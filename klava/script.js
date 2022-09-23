b2c0b94f79abb3a0bd96088faf9b9875788af91a
Dannicser <112568847+Dannicser@users.noreply.github.com>
2022-09-23 13:51:29 +0300

    Add files via upload

---
+++ klava/script.js
@@ -0,0 +1,20 @@
+const leter = document.querySelectorAll(".leter_js");
+const out = document.querySelector(".out");
+const inp = document.querySelector("#i-1");
+
+inp.onkeypress = (event) => {
+  for (let item of leter) {
+    if (event.code == "Key" + item.textContent.toUpperCase()) {
+      item.classList.add("change");
+      setTimeout(del, 200);
+    }
+
+    if (event.code == item.textContent && item.textContent.length > 4) {
+      item.classList.add("change_");
+      setTimeout(del, 200);
+    }
+    function del() {
+      item.classList.remove("change", "change_");
+    }
+  }
+};
